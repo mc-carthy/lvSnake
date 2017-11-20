55db07bf71a04e7fadf39b6de091f862e5eb7aaa
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-20 21:27:05 +0000

    Snake is moving

--- main.lua
+++ main.lua
@@ -1,9 +1,27 @@
 function love.load()
+    timer = 0
 
+    snakeSegments = {
+        { x = 3, y = 1},
+        { x = 2, y = 1},
+        { x = 1, y = 1},
+    }
 end
 
 function love.update(dt)
+    timer = timer + dt
+    local timerLimit = 0.15
+    if timer >= timerLimit then
+        timer = timer - timerLimit
 
+        local nextXPos = snakeSegments[1].x + 1
+        local nextYPos = snakeSegments[1].y
+
+        table.insert(snakeSegments, 1, { x = nextXPos, y = nextYPos })
+        table.remove(snakeSegments)
+
+        print('Tick')
+    end
 end
 
 function love.draw()
@@ -13,12 +31,6 @@ function love.draw()
 
     love.graphics.setColor(63, 63, 63)
     love.graphics.rectangle('fill', 0, 0, gridXCount * cellSize, gridYCount * cellSize)
-
-    local snakeSegments = {
-        { x = 3, y = 1},
-        { x = 2, y = 1},
-        { x = 1, y = 1},
-    }
 
     for segmentIndex, segment in ipairs(snakeSegments) do
         love.graphics.setColor(95, 191, 95)

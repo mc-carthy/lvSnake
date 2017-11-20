4c7fe72fae9b737e36ff6a850e12ef58fb9bb06a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-20 21:23:54 +0000

    Draw basic scene

--- main.lua
+++ main.lua
@@ -7,5 +7,27 @@ function love.update(dt)
 end
 
 function love.draw()
+    local gridXCount = 20
+    local gridYCount = 15
+    local cellSize = 35
 
+    love.graphics.setColor(63, 63, 63)
+    love.graphics.rectangle('fill', 0, 0, gridXCount * cellSize, gridYCount * cellSize)
+
+    local snakeSegments = {
+        { x = 3, y = 1},
+        { x = 2, y = 1},
+        { x = 1, y = 1},
+    }
+
+    for segmentIndex, segment in ipairs(snakeSegments) do
+        love.graphics.setColor(95, 191, 95)
+        love.graphics.rectangle(
+            'fill',
+            (segment.x - 1) * cellSize,
+            (segment.y - 1) * cellSize,
+            cellSize - 1,
+            cellSize - 1
+        )
+    end
 end
